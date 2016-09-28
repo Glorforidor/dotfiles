785f0491304293a53fca1782fe5b2702370daed2
Peter Bom Jakobsen <pejak1990@gmail.com>
2016-09-28 16:33:22 +0200

    Added go-appengine and Rust installer to script.

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+# Setup Vim and Tmux
 git submodule init
 git submodule update
-#Sleep for a bit.
+# Sleep for a bit.
 sleep 2
 cp -r .vim .vimrc ~/
 cp -r .tmux .tmux.conf ~/
-wget -O ~/Downloads/go.tar.gz https://storage.googleapis.com/golang/go1.6.0.linux-amd64.tar.gz
-#Sleep for a bit.
+
+# Setup Golang
+wget -O ~/Downloads/go.tar.gz https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+# Sleep for a bit.
 sleep 2
 sudo tar -C /usr/local -xzvf ~/Downloads/go.tar.gz
 mkdir -p ~/Programming/Go/bin
 mkdir -p ~/Programming/Go/pkg
 mkdir -p ~/Programming/Go/src
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
-echo "export GOPATH=\$HOME/Programming/Go" >> ~/.profile
+echo "export PATH=\$PATH:/usr/local/go/bin" >> $HOME/.profile
+echo "export GOPATH=\$HOME/Programming/Go" >> $HOME/.profile
+echo "export PATH=\$PATH:\$GOPATH/bin" >> $HOME/.profile
 
+# Setup Rust
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
+
+# Setup Google App Engine
+echo "export PATH=\$PATH:\$HOME/go_appengine" >> $HOME/.profile
