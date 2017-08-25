1eafa161a4c8891fcfb1d66173f499965f0e7982
Peter Bom Jakobsen <pejak1990@gmail.com>
2017-08-25 09:39:32 +0200

    uncommented things I don't need by default.

--- install.sh
+++ install.sh
@@ -25,39 +25,36 @@ cp -r .vim .vimrc $HOME/
 cp -r .tmux .tmux.conf $HOME/
 
 # Download Golang
-wget -O $HOME/Downloads/go.tar.gz https://storage.googleapis.com/golang/go1.8.1.linux-amd64.tar.gz
+wget -O $HOME/Downloads/go.tar.gz https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
 
 # Sleep for a bit.
 sleep 2
 
 # Setup Golang
 sudo tar -C /usr/local -xzvf $HOME/Downloads/go.tar.gz
-mkdir -p $HOME/programming/go/bin
-mkdir -p $HOME/programming/go/pkg
-mkdir -p $HOME/programming/go/src
 echo '# set Go
 export PATH="$PATH:/usr/local/go/bin"
-export GOPATH="$HOME/programming/go"
-export PATH="$PATH:$GOPATH/bin"
+export PATH="$PATH:$HOME/go/bin"
 ' >> $HOME/.profile
 
 # Download Rust
-echo '# set Rust'
-curl https://sh.rustup.rs -sSf | sh
+# echo '# set Rust'
+# curl https://sh.rustup.rs -sSf | sh
 
 # Download Google App Engine
-wget -O $HOME/Downloads/goappengine.zip https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.48.zip
+# wget -O $HOME/Downloads/goappengine.zip https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.48.zip
 
 # Sleep for a bit
-sleep 2
+# sleep 2
 
 # Setup Google App Engine
-unzip $HOME/Downloads/goappengine.zip -d $HOME/
-echo '# set google app engine for go
-export PATH="$PATH:$HOME/go_appengine"
-' >> $HOME/.profile
+# unzip $HOME/Downloads/goappengine.zip -d $HOME/
+# echo '# set google app engine for go
+# export PATH="$PATH:$HOME/go_appengine"
+# ' >> $HOME/.profile
 
 # Install Oh My Zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 cp -r .zshrc $HOME/
+cp $HOME/.profile $HOME/.zprofile
