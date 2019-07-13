57fb3df979b12f853ffef077bd8fee4dc1da6008
Peter Bom Jakobsen <pejak1990@gmail.com>
2019-07-13 09:23:55 +0200

    beautify

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Setup linux installations after my preference.
 
 # source the os-release so the package manager can be detected
-if [[ -f /etc/os-release ]]; then
+if [[ -f '/etc/os-release' ]]; then
     source '/etc/os-release'
 fi
 
@@ -20,63 +20,62 @@ case "${ID}" in
         ;;
 esac
 
-if [[ ! -f /bin/zsh ]]; then
+if [[ ! -f '/bin/zsh' ]]; then
     sudo "${package_manager}" install -y zsh
 fi
 
-if [[ ! -f /usr/bin/curl ]]; then
+if [[ ! -f '/usr/bin/curl' ]]; then
     sudo "${package_manager}" install -y curl
 fi
 
-if [[ ! -f /usr/bin/tmux ]]; then
+if [[ ! -f '/usr/bin/tmux' ]]; then
     sudo "${package_manager}" install -y tmux
 fi
 
 # Setup Vim and Tmux
-git submodule init
-git submodule update
+git submodule update --init --recursive
 
 # Sleep for a bit.
 sleep 2
 
-cp -r .vim .vimrc "${HOME}"/
-cp -r .tmux .tmux.conf "${HOME}"/
+cp -r .vim .vimrc "${HOME}/"
+cp -r .tmux .tmux.conf "${HOME}/"
 
 # Download Go
-curl --create-dirs -o "${HOME}"/Downloads/go.tar.gz \
+curl --create-dirs -o "${HOME}/Downloads/go.tar.gz" \
     https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
 
 # Sleep for a bit.
 sleep 2
 
 # Setup Go
-if [[ ! -d /usr/local/go ]]; then
-    sudo tar -C /usr/local -xzvf "${HOME}"/Downloads/go.tar.gz
+if [[ ! -d '/usr/local/go' ]]; then
+    sudo tar -C '/usr/local' -xzvf "${HOME}/Downloads/go.tar.gz"
 fi 
 
-grep -qxF '# set Go' ~/.profile || echo '# set Go
-export PATH="${PATH}":/usr/local/go/bin
-export PATH="${PATH}":"${HOME}"/go/bin
-' >> "${HOME}"/.profile
+grep -qxF '# set Go' '~/.profile' || echo '# set Go
+export PATH="${PATH}:/usr/local/go/bin"
+export PATH="${PATH}:${HOME}"/go/bin"
+' >> "${HOME}/.profile"
 
 # Download Rust
 # echo '# set Rust'
 # curl https://sh.rustup.rs -sSf | sh
 
 # Download Google App Engine
-# curl --create-dirs -o "${HOME}"/Downloads/goappengine.zip https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.48.zip
+# curl --create-dirs -o "${HOME}/Downloads/goappengine.zip" https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.48.zip
 
 # Sleep for a bit
 # sleep 2
 
 # Setup Google App Engine
-# unzip "${HOME}"/Downloads/goappengine.zip -d "${HOME}"/
+# unzip "${HOME}/Downloads/goappengine.zip" -d "${HOME}/"
 # echo '# set google app engine for go
-# export PATH="${PATH}":"${HOME}"/go_appengine
-# ' >> "${HOME}"/.profile
+# export PATH="${PATH}:${HOME}/go_appengine"
+# ' >> "${HOME}/.profile"
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-cp -r .zshrc "${HOME}"/
-cp "${HOME}"/.profile "${HOME}"/.zprofile
+cp -r .zshrc "${HOME}/"
+cp "${HOME}/.profile" "${HOME}/.zprofile"
